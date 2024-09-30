1c499a1bdeb62845678ac360e278e7745cf1a4c9
vinayak teradali <vteradali5@gmail.com>
2024-09-30 22:26:51 +0530

    angular initial setup

---
+++ mean-stack/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostCreateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ mean-stack/src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrl: './post-create.component.css'
+})
+export class PostCreateComponent implements OnInit {
+
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+
+  logs(){
+
+  }
+
+}
